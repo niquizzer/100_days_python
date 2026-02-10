c1dae86513bd19362c2a72136abe50c0854f2a68
Nequea <nequeacoleman@gmail.com>
2026-02-09 20:27:57 -0500

    more RPS basic stuff, experimentith with match/case

--- Random_And_Lists/RPS.py
+++ Random_And_Lists/RPS.py
@@ -1,9 +1,12 @@
 # Rock, Paper, Scissors
 
-rock = 'R'
-paper = 'P'
-scissors = 'S'
-
 print("What do you choose? 0 for rock, 1 for paper, 2 for scissors")
 choice = input()
 
+match choice:
+    case "0":
+        print("You chose rock!")
+    case "1":
+        print("You chose paper!")
+    case "2":
+        print("You chose scissors!")
